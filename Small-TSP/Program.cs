72d82128587303b260ac3fb33193ecf84ca6d08f
Mikhail Kharitonov <95705443+MAKharitonov@users.noreply.github.com>
2023-10-24 14:25:50 +0300

    add_mask_routes

--- Small-TSP/Program.cs
+++ Small-TSP/Program.cs
@@ -18,7 +18,7 @@ class Program
 {
     public static void Main(string[] args)
     {
-        string fileName = "/home/mikhail/RiderProjects/SmallTSP/data/111.json";
+        string fileName = "C:\\Users\\mikhail\\Axelot\\Small-TSP\\data\\111.json";//"/home/mikhail/RiderProjects/SmallTSP/data/111.json";
         IFileManager fileManager = new FileManager();
         string data = fileManager.Read(fileName);
         ISerializer serializer = new JsonDataManager();
@@ -26,24 +26,19 @@ class Program
         List<ArcImprovedRoute> distanceMatrix = JsonConvert.DeserializeObject<List<ArcImprovedRoute>>(data);
 
         SolverORTools solver = new SolverORTools();
-        string maskStart = "55,809762_37,392311";
+        string maskStart = "55,809762_37,392311";//"55,730715_37,395483";//
         string maskEnd = "55,764592_37,877805";
-        /*
-        (int [,] dist, int start, int end)  = solver.BuildRouteData(distanceMatrix, maskStart, maskEnd);
 
-        for (int i = 0; i < 16; i++)
-        {
-            for (int j = 0; j < 16; j++)
-            {
-                Console.Write($"{dist[i,j]}\t");
-            }
-            Console.Write("\n");
-        }
-        */
        
-        (int[,] solution, long objective) = solver.GetSolution(distanceMatrix, maskStart, maskEnd);
+        List<int> solution = solver.GetMaskRoutePoints(distanceMatrix, maskStart, maskEnd);
+        Console.WriteLine($"\nMy solution");
+        foreach (int item in solution)
+        {
+            Console.Write($"{item} -> ");
+        }
+        
 
-        Console.WriteLine($"{objective}");
+        //Console.WriteLine($"{objective}");
 
     }
 }
